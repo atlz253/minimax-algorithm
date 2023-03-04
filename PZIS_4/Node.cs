0592a7ff35fd0515930cb7a9120d0c7c11e55098
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2023-03-04 13:11:06 +0300

    Добавил ссылку на родительский узел

--- PZIS_4/Node.cs
+++ PZIS_4/Node.cs
@@ -9,6 +9,8 @@ namespace PZIS_4
     {
         int value = int.MaxValue; // int.MaxValue - у узла неизвестное значение
 
+        private Node? parent;
+
         private readonly List<Node> childrens = new();
 
         /// <summary>
@@ -17,15 +19,35 @@ namespace PZIS_4
         public IReadOnlyList<Node> Childrens => childrens;
 
         /// <summary>
+        /// Родительский узел
+        /// </summary>
+        public Node? Parent
+        {
+            get => parent;
+
+            private set => parent = value;
+        }
+
+        /// <summary>
         /// Добавляет дочерний узел
         /// </summary>
         /// <param name="node">Узел для добавления</param>
-        public void Add(Node node) => childrens.Add(node);
+        public void Add(Node node)
+        {
+            childrens.Add(node);
+            
+            node.Parent = this;
+        }
 
         /// <summary>
         /// Удаляет дочерний узел
         /// </summary>
         /// <param name="node">Узел для добавления</param>
-        public void Remove(Node node) => childrens.Remove(node);
+        public void Remove(Node node)
+        {
+            childrens.Remove(node);
+
+            node.Parent = null;
+        }
     }
 }
